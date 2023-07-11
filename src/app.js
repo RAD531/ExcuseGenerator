50f63a701b21e0935d9086c6e525a4475b940411
RAD531 <69318839+RAD531@users.noreply.github.com>
2023-07-11 14:06:42 +0000

    Updated Excuse Generator Code

--- src/app.js
+++ src/app.js
@@ -5,7 +5,30 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+$(document).ready(function() {
+  $("#excuseButton").click(function() {
+    onload();
+  });
+
+  function onload() {
+    const elem = document.getElementById("excuse");
+
+    let who = ["The dog", "My grandma", "His turtle", "My bird"];
+    let action = ["ate", "peed", "crushed", "broke"];
+    let what = ["my homework", "the keys", "the car"];
+    let when = [
+      "before the class",
+      "right on time",
+      "when I finished",
+      "during my lunch",
+      "while I was praying"
+    ];
+
+    var whoText = who[Math.floor(Math.random() * who.length)];
+    var actionText = action[Math.floor(Math.random() * action.length)];
+    var whatText = what[Math.floor(Math.random() * what.length)];
+    var whenText = when[Math.floor(Math.random() * when.length)];
+
+    elem.innerHTML = `${whoText} ${actionText} ${whatText} ${whenText}`;
+  }
+});
